d6432bf61e37f8c3848954bbd4519486e4b2ce7a
Marcus Nasarek <m.nasarek@gmx.de>
2013-10-11 22:36:40 +0200

    finished issue_35; 3 measures: input validation (2 levels: medium -> just escape html instructions, secure: filter only valid strings  from input), hidden input field as a honeypot for bots, and a confirmation page to slow down manual form fillings

--- controllers/disc_jockey_controller.rb
+++ controllers/disc_jockey_controller.rb
@@ -19,7 +19,6 @@ class DiscJockeyController < ApplicationController
 
   get '/hintergrund' do  
     @hintergr = ['Bar Jazz','Cafe del Mar', 'Chanson', 'Klassik', 'Kuba', 'Lounge', 'Aktuelles']
-    puts @hintergr[0]
     @descriptions = {
       @hintergr[0] => "Bar Jazz ist...",
       @hintergr[1] => "Cafe del Mar ist...",
@@ -92,68 +91,72 @@ class DiscJockeyController < ApplicationController
   end
 
   post '/kundendaten' do
-    @db = DBManager.new
-    # Get the parameters for further parsing
-    name = params[:name]
-    email = params[:email]
-    tel = params[:tel]
-    datum = params[:datum]
-    zeit = params[:zeit]
-    strasse = params[:strasse]
-    stadt = params[:stadt]
-    anzahl = params[:anzahl]
-    anzahl20 = params[:unter20]
-    anzahl60 = params[:ueber60]
-    equipment = params[:equipment]
-    beratung = params[:beratung]
-    kommentar = params[:message] 
 
+    redirect '/' unless params[:plz].empty?
+
+    @db = DBManager.new
+    # deklare with empty strings
+    name = ""
+    email = ""
+    tel = ""
+    datum = ""
+    zeit = ""
+    strasse = ""
+    stadt = ""
+    anzahl = ""
+    anzahl20 = ""
+    anzahl60 = ""
+    equipment = ""
+    beratung = ""
+    kommentar = ""
+
+    # FIXME: Set hardening level per environment in config or as an option
     hardening = :medium
 
     case hardening 
     when :secure
-      name = params[:name].filter_purpose(:name)
-      email = params[:email].filter_purpose(:email)
-      tel = params[:tel].filter_purpose(:tel)
-      datum = params[:datum].filter_purpose(:datum)
-      zeit = params[:zeit].filter_purpose(:zeit)
-      strasse = params[:strasse].filter_purpose(:street)
-      stadt = params[:stadt].filter_purpose(:city)
-      anzahl = params[:anzahl].filter_purpose(:numbers)
-      anzahl20 = params[:unter20].filter_purpose(:numbers)
-      anzahl60 = params[:ueber60].filter_purpose(:numbers)
-      equipment = params[:equipment].filter_purpose(:word)
-      beratung = params[:beratung].filter_purpose(:word)
+      name = params[:name].filter_purpose(:name) unless params[:name].nil?
+      email = params[:email].filter_purpose(:email) unless params[:email].nil?
+      tel = params[:tel].filter_purpose(:tel) unless params[:tel].nil?
+      datum = params[:datum].filter_purpose(:datum) unless params[:datum].nil?
+      zeit = params[:zeit].filter_purpose(:zeit) unless params[:zeit].nil?
+      strasse = params[:strasse].filter_purpose(:street) unless params[:strasse].nil?
+      stadt = params[:stadt].filter_purpose(:city) unless params[:stadt].nil?
+      anzahl = params[:anzahl].filter_purpose(:numbers) unless params[:anzahl].nil?
+      anzahl20 = params[:unter20].filter_purpose(:numbers) unless params[:unter20].nil?
+      anzahl60 = params[:ueber60].filter_purpose(:numbers) unless params[:ueber60].nil?
+      equipment = params[:equipment].filter_purpose(:word) unless params[:equipment].nil?
+      beratung = params[:beratung].filter_purpose(:word) unless params[:beratung].nil?
       # Here is a vulnerability:
-      kommentar = CGI::escape_html(params[:message])
+      kommentar = CGI::escape_html(params[:message]) unless params[:beratung].nil?
     when :medium
-      name = CGI::escape_html(params[:name])
-      email = CGI::escape_html(params[:email])
-      tel = CGI::escape_html(paramsCGI::escape_html([:tel])
-      datum = CGI::escape_html(params[:datum])
-      zeit = CGI::escape_html(params[:zeit])
-      strasse = CGI::escape_html(params[:strasse])
-      stadt = CGI::escape_html(params[:stadt])
-      anzahl = CGI::escape_html(params[:anzahl])
-      anzahl20 = CGI::escape_html(params[:unter20])
-      anzahl60 = CGI::escape_html(params[:ueber60])
-      equipment = CGI::escape_html(params[:equipment])
-      beratung = CGI::escape_html(params[:beratung])
-      kommentar = CGI::escape_html(params[:message])
+      name = CGI::escape_html(params[:name]) unless params[:name].nil?
+      email = CGI::escape_html(params[:email]) unless params[:email].nil?
+      tel = CGI::escape_html(params[:tel]) unless params[:tel].nil?
+      datum = CGI::escape_html(params[:datum]) unless params[:datum].nil?
+      zeit = CGI::escape_html(params[:zeit]) unless params[:zeit].nil?
+      strasse = CGI::escape_html(params[:strasse]) unless params[:strasse].nil?
+      stadt = CGI::escape_html(params[:stadt]) unless params[:stadt].nil?
+      anzahl = CGI::escape_html(params[:anzahl]) unless params[:anzahl].nil?
+      anzahl20 = CGI::escape_html(params[:unter20]) unless params[:unter20].nil?
+      anzahl60 = CGI::escape_html(params[:ueber60]) unless params[:ueber60].nil?
+      equipment = CGI::escape_html(params[:equipment]) unless params[:equipment].nil?
+      beratung = CGI::escape_html(params[:beratung]) unless params[:beratung].nil?
+      kommentar = CGI::escape_html(params[:message]) unless params[:message].nil?
     end      
     
-    surname = (params[:name]).split(" ") unless params[:name].nil?
-    lastname = surname.last unless surname.nil?
-    surname.delete(lastname) unless lastname.nil?
+    surname = (name).split(" ")
+    lastname = surname.last.nil? ? "" : surname.last 
+    surname.delete(lastname) unless lastname.empty?
     surname = surname.join(" ")
 
-    @u = User.find(:first, :conditions => [ "email = ?", params[:email]])
+    @u = User.find(:first, :conditions => [ "email = ?", email])
     
     @u = User.create(
       :name => lastname,
       :firstname => surname,
-      :email  => params[:email],
-      :tel => params[:tel],
+      :email  => email,
+      :tel => tel,
       :role => 'user') if @u.nil?
 
     @w = Wish.create(
@@ -163,27 +166,79 @@ class DiscJockeyController < ApplicationController
       :user_id => @u.id)
 
     @er = Event.new do |r|
-      r.datum = params[:datum]
-      r.zeit = params[:zeit]
-      r.strasse = params[:strasse]
-      r.stadt = params[:stadt]
-      r.anzahl = params[:anzahl]
-      r.anzahl20 = params[:unter20]
-      r.anzahl60 = params[:ueber60]
-      r.equipment = params[:equipment]
-      r.beratung = params[:beratung]
-      r.kommentar = params[:message]
+      r.datum = datum
+      r.zeit = zeit
+      r.strasse = strasse
+      r.stadt = stadt
+      r.anzahl = anzahl
+      r.anzahl20 = anzahl20
+      r.anzahl60 = anzahl60
+      r.equipment = equipment
+      r.beratung = beratung
+      r.kommentar = kommentar
       r.user_id = @u.id
       r.wish_id = @w.id
     end
     @er.save
+    
+    session[:event_id] = @er.id
 
-    redirect "/abschluss?id=#{@er.id}"
+    redirect "/confirm"
+  end
+
+  get '/confirm' do
+    @id = session[:event_id]
+
+    unless @id.nil? then
+      @db = DBManager.new
+      @event = Event.find(@id)
+      unless @event.nil? then
+        @user = User.find(@event.user_id)
+        @wish = Wish.find(@event.wish_id)
+
+        @event_date = Time.parse(@event.datum.to_s).strftime("%d. %b %Y") unless @event.datum.nil?
+        @event_time = Time.parse(@event.zeit.to_s).strftime("%H:%M") unless @event.zeit.nil?
+      end
+    end
+
+    haml :confirm
+  end
+
+  post '/confirm' do
+    redirect '/abschluss'
+  end
+
+  post '/cancel' do
+    @id = session[:event_id]
+
+    unless @id.nil? then
+      @db = DBManager.new
+
+      @event = Event.find(@id)
+
+      unless @event.nil? then
+        @user = User.find(@event.user_id)
+
+        Wish.destroy(@event.wish_id)
+        Event.destroy(@id)
+        User.destroy(@user.id) if @user.events.empty?
+
+        logger.info "event #{@id}: #{@user.name} ge-cancelt und gelöscht"
+      end
+    end
+
+    #FIXME: Hinweis, dass abgebrochen wurde
+    redirect '/'
   end
     
   get '/abschluss' do
-    @id = params[:id]
+    @id = session[:event_id]
 
+    if @id.nil? then 
+      #FIXME: Hinweis, dass das Event nicht gefunden wurde 
+      redirect '/'
+    end
+    
     @db = DBManager.new
     @event = Event.find(@id)
     @user = User.find(@event.user_id)
