10d2f80935b3932a748347b8a5990c8cc25e652b
Rheik van Eyck <rheikvaneyck@yahoo.de>
2013-10-13 21:21:40 +0200

    added rackup task

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'wishmemusic'
-set :repo_url, 'file:///home/nasarek/source/WishMeMusic'
+set :repo_url, 'git@github.com:rheikvaneyck/WishMeMusic.git'
 # set :repo_url, 'git@example.com:me/my_repo.git'
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
@@ -10,7 +10,7 @@ set :scm, :git
 # set :log_level, :debug
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/email.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -18,11 +18,23 @@ set :keep_releases, 5
 
 namespace :deploy do
 
+  desc 'Building gems'
+  task :build_gems, :roles => :app do
+      run "cd #{release_path} && bundle install --deployment"
+  end
+      
+  desc "Migrating database"
+  task :migrations do
+      run "cd #{release_path} && bundle exec rake db:migrate"
+  end
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      find_and_execute_task("rackup:stop")
+      find_and_execute_task("rackup:start")
     end
   end
 
